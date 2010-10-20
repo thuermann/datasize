1d495a469ac207e1b38444debc3bc9a588d10f5c
Urs Thuermann <urs@isnogud.escape.de>
2010-10-20 13:09:13 +0200

    Add sizes of time_t, size_t, and off_t

--- datasize.c
+++ datasize.c
@@ -1,8 +1,10 @@
 /*
- * $Id: datasize.c,v 1.3 2010/10/20 11:09:03 urs Exp $
+ * $Id: datasize.c,v 1.4 2010/10/20 11:09:13 urs Exp $
  */
 
 #include <stdio.h>
+#include <time.h>
+#include <sys/types.h>
 
 #define FMTSIZE(type) "%-12s %2zu\n", #type, sizeof(type)
 
@@ -13,7 +15,14 @@ int main(void)
 	printf(FMTSIZE(int));
 	printf(FMTSIZE(long));
 	printf(FMTSIZE(long long));
+	putchar('\n');
+
+	printf(FMTSIZE(time_t));
+	printf(FMTSIZE(size_t));
+	printf(FMTSIZE(off_t));
 	printf(FMTSIZE(void *));
+	putchar('\n');
+
 	printf(FMTSIZE(float));
 	printf(FMTSIZE(double));
 	printf(FMTSIZE(long double));
