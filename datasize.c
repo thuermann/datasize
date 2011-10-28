6b864c5a48f0a2643df31b1ddb845ac356a514c1
Urs Thuermann <urs@isnogud.escape.de>
2011-10-28 08:56:47 +0200

    Add clock_t and some struct members

--- datasize.c
+++ datasize.c
@@ -1,15 +1,20 @@
 /*
- * $Id: datasize.c,v 1.4 2010/10/20 11:09:13 urs Exp $
+ * $Id: datasize.c,v 1.5 2011/10/28 06:56:47 urs Exp $
  */
 
 #include <stdio.h>
 #include <time.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 
 #define FMTSIZE(type) "%-12s %2zu\n", #type, sizeof(type)
 
 int main(void)
 {
+	struct timeval  tv;
+	struct timespec ts;
+	struct stat     st;
+
 	printf(FMTSIZE(char));
 	printf(FMTSIZE(short));
 	printf(FMTSIZE(int));
@@ -21,6 +26,15 @@ int main(void)
 	printf(FMTSIZE(size_t));
 	printf(FMTSIZE(off_t));
 	printf(FMTSIZE(void *));
+	printf(FMTSIZE(clock_t));
+	putchar('\n');
+
+	printf(FMTSIZE(tv.tv_sec));
+	printf(FMTSIZE(tv.tv_usec));
+	printf(FMTSIZE(st.st_size));
+	printf(FMTSIZE(st.st_atime));
+	printf(FMTSIZE(ts.tv_sec));
+	printf(FMTSIZE(ts.tv_nsec));
 	putchar('\n');
 
 	printf(FMTSIZE(float));
